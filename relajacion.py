8173763d498ac95593a0ac9798c8d2deec49e8d8
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2019-11-10 14:03:13 -0600

    SOR method was updated

--- relajacion.py
+++ relajacion.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def relajacion(A, b, omega, x0, tol):
+def relajacion(A, b, x0, omega=0.5, tol=1e-8):
     """
     This is an implementation of the succesive over-relaxation method.
 
@@ -21,7 +21,7 @@ def relajacion(A, b, omega, x0, tol):
     
       x: solution matrix
     """
-    x = x0[:]
+    x = x0.copy()
     residual = np.linalg.norm(np.matmul(A, x) - b)
     while residual > tol:
         for i in range(A.shape[0]):
@@ -33,46 +33,17 @@ def relajacion(A, b, omega, x0, tol):
         residual = np.linalg.norm(np.matmul(A, x) - b)
     return x
 
+
 if __name__ == '__main__':
-    # == ESTE ES UN EJEMPLO DE USO, NO ADJUNTAR AL ENTREGABLE ==
-    tol = 1e-8
-    omega = 0.5
+    print('SOR method working, put your code below')
+    # Example, uncomment to see
+    # A = np.array([[ 4, -1, -6,  0],
+    #            [-5, -4, 10,  8],
+    #            [ 0,  9,  4, -2],
+    #            [ 1,  0, -7,  5]])
+    # b = np.array([2, 21, -12, -6])
+    # x0 = np.zeros(4)
+    # print(A, b, x0, '', sep='\n')
 
-    A = np.ones((4, 4))
-    A[0][0] = 4
-    A[0][1] = -1
-    A[0][2] = -6
-    A[0][3] = 0
-
-    A[1][0] = -5
-    A[1][1] = -4
-    A[1][2] = 10
-    A[1][3] = 8
-
-    A[2][0] = 0
-    A[2][1] = 9
-    A[2][2] = 4
-    A[2][3] = -2
-
-    A[3][0] = 1
-    A[3][1] = 0
-    A[3][2] = -7
-    A[3][3] = 5
-
-    b = np.ones(4)
-    b[0] = 2
-    b[1] = 21
-    b[2] = -12
-    b[3] = -6
-
-    x0 = np.zeros(4)
-
-    print(A)
-    print('\n')
-    print(b)
-    print('\n')
-    print(x0)
-    print('\n')
-
-    x = relajacion(A, b, omega, x0, tol)
-    print(x)
+    # x = relajacion(A, b, x0)
+    # print(x)
